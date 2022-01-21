7c2f01d6fa07a5fe5052fbd7b0e1150436839853
Rahul Sahu <55999865+therahulsahu@users.noreply.github.com>
2022-01-21 23:15:38 +0530

    Time: 2 ms (13.21%), Space: 40.9 MB (15.37%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -4,27 +4,27 @@ class Solution {
         
         while(start <= end) {
             mid = start + (end - start) / 2;
-            
             if(nums[mid] == target) {
                 return true;
             }
             
-            if( (nums[start] == nums[mid]) && (nums[end] == nums[mid]) ) {
-                ++start;
-                --end;
-            }else if(nums[start] <= nums[mid]) {
-                if((nums[start] <= target) && (target < nums[mid])) {
+            if(nums[mid] == nums[start] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+            } else if(nums[mid] >= nums[start]) {
+                if(target >= nums[start] && target < nums[mid]) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             } else {
-                if((target <= nums[end]) && (target > nums[mid])) {
+                if(target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
                 }
             }
+            
         }
         return false;
     }
