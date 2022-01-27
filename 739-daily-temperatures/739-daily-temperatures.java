8f5413dc891c48f5d2bf3ca16e2a41a79602d76a
Rahul Sahu <55999865+therahulsahu@users.noreply.github.com>
2022-01-27 22:43:31 +0530

    Time: 223 ms (12.37%), Space: 130.2 MB (5.43%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n]; // will contain index to next greater element
+        
+        Stack<Integer> stack = new Stack<>(); // will contain index
+        
+        for(int i = n - 1; i >= 0; i--) {
+            if(stack.isEmpty()) {
+                result[i] = i;
+            } else if(temperatures[stack.peek()] > temperatures[i]) {
+                result[i] = stack.peek();
+            } else {
+                while(stack.size() > 0 && temperatures[stack.peek()] <= temperatures[i]) {
+                    stack.pop();
+                }
+                
+                if(stack.size() > 0) {
+                    result[i] = stack.peek();
+                } else {
+                    result[i] = i;
+                }
+            }
+            stack.push(i);
+        }
+        
+        for(int i = 0; i < result.length; i++) {
+            result[i] = result[i] - i; // nextGreaterElementIdx - itself index; = no. of days
+        }
+        return result;
+    }
+}
