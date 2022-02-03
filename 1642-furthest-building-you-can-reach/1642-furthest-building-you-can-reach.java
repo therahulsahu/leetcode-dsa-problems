ac3c33f7e14ded4d479a43d66f305a896507514a
Rahul Sahu <55999865+therahulsahu@users.noreply.github.com>
2022-02-03 20:21:37 +0530

    Time: 51 ms (11.90%), Space: 78.9 MB (7.54%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,22 +1,19 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        int sum = 0, nLadder = 0;
-        int i = 0;
-        for(i = 1; i < heights.length; i++) {
-            if(heights[i] > heights[i - 1]) {
-                sum += heights[i] - heights[i - 1];
-                maxHeap.add(heights[i] - heights[i-1]);
-                if(sum > bricks) {
-                    if(nLadder < ladders) {
-                        sum -= maxHeap.poll(); 
-                        nLadder++;
-                    } else {
-                        break;
-                    }
-                }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int nBricks = 0;
+        for(int i = 0; i < heights.length - 1; i++) {
+            int d = heights[i + 1] - heights[i];
+            if(d > 0) {
+                minHeap.add(d);
+            }
+            if(minHeap.size() > ladders) {
+                nBricks += minHeap.poll();
+            }
+            if(nBricks > bricks) {
+                return i;
             }
         }
-        return i - 1;
+        return heights.length - 1;
     }
 }
