51c171b459e5c0be03c0caccc37cd64101231a50
Rahul Sahu <55999865+therahulsahu@users.noreply.github.com>
2022-02-13 20:42:42 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (23.91%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,18 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        solve(nums, temp, 0, answer);
-        return answer;
-    }
-    private void solve(int[] nums, List<Integer> temp, int index, List<List<Integer>> answer) {
-        if(index == nums.length) {
-            answer.add(new ArrayList<>(temp));
-            return;
+        answer.add(new ArrayList<Integer>());
+        for(int n: nums) {
+            int size = answer.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(answer.get(i));
+                subset.add(n);
+                answer.add(subset);
+            }
         }
-        solve(nums, temp, index + 1, answer);
-        temp.add(nums[index]);
-        solve(nums, temp, index + 1, answer);
-        temp.remove(temp.size() - 1);
+        return answer;
     }
 }
